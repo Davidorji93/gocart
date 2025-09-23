6203c43a1aef77cf8addac5e11b27553e1c532eb
David Orji <davidorji93@gmail.com>
2025-09-23 11:23:27 +0100

    changes

--- app/api/admin/coupon/route.js
+++ app/api/admin/coupon/route.js
@@ -16,7 +16,16 @@ export async function POST(request) {
     const { coupon } = await request.json();
     coupon.code = coupon.code.toUpperCase();
 
-    await prisma.coupon.create({ data: coupon });
+    await prisma.coupon.create({ data: coupon }).then(async (coupon) => {
+      // Trigger inngest function to delete coupon on expiry
+      await inngest.send({
+        name: "delete-coupon-on-expiry",
+        data: {
+          code: coupon.code,
+          expires_at: coupon.expiresAt,
+        },
+      });
+    });
 
     return NextResponse.json(
       { message: "Coupon created successfully" },
@@ -39,13 +48,13 @@ export async function DELETE(request) {
     if (!isAdmin) {
       return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
     }
-    
+
     const { searchParams } = request.nextUrl;
-    const code = searchParams.get("code"); 
+    const code = searchParams.get("code");
 
     await prisma.coupon.delete({
       where: { code },
-    }); 
+    });
 
     return NextResponse.json(
       { message: "Coupon deleted successfully" },
@@ -57,10 +66,8 @@ export async function DELETE(request) {
       { status: 500 }
     );
   }
-
 }
 
- 
 // Get all coupons
 export async function GET(request) {
   try {
